62d5cc0e2177b01be647553909b18f93fc5acbaa
akammahmodi <akammahmodi88@gmail.com>
2021-05-25 15:29:13 +0200

    working on Step 6: Send the e-mail

--- form-view.php
+++ form-view.php
@@ -7,9 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
           rel="stylesheet"/>
+    <link rel="stylesheet" href="style.css">
+    <link>
     <title>Order food & drinks</title>
 </head>
 <body>
+<div class="alert alert-success text-center" role="alert">
+    <?php
+    if (isset($submit)) {
+        if (isset($error)) {
+            echo $error;
+        }
+    }
+    ?>
+</div>
 <div class="container">
     <h1>Order food in restaurant "the Personal Ham Processors"</h1>
     <nav>
@@ -26,7 +37,7 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $email ?>">
             </div>
             <div></div>
         </div>
@@ -37,40 +48,42 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street ?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                           value="<?php echo $streetNumber ?>">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city ?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipCode ?>">
                 </div>
             </div>
         </fieldset>
 
         <fieldset>
             <legend>Products</legend>
-            <?php foreach ($products AS $i => $product): ?>
+            <?php foreach ($products as $i => $product): ?>
                 <label>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?>
+                    -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br/>
             <?php endforeach; ?>
         </fieldset>
-        
+
         <label>
-            <input type="checkbox" name="express_delivery" value="5" /> 
-            Express delivery (+ 5 EUR) 
+            <input type="checkbox" name="express_delivery" value="5"/>
+            Express delivery (+ 5 EUR)
         </label>
-            
-        <button type="submit" class="btn btn-primary">Order!</button>
+
+        <button type="submit" class="btn btn-primary" name="submit">Order!</button>
     </form>
 
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 //we are going to use session variables so we need to enable sessions
 session_start();
 
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -16,22 +20,137 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
+//whatIsHappening();
+
 //your products with their price.
-$products = [
-    ['name' => 'Club Ham', 'price' => 3.20],
-    ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
-    ['name' => 'Club Chicken', 'price' => 4],
-    ['name' => 'Club Salmon', 'price' => 5]
-];
+if ((isset($_GET["food"])) && ($_GET["food"] == 0)) {
 
-$products = [
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-];
+    $products = [
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 3],
+    ];
 
+} else {
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5]
+    ];
+
+}
+
+//https://www.php.net/manual/en/function.strtotime.php
 $totalValue = 0;
+$delivery_time = date("H:i:s", strtotime("+2 Hours"));
+$deliveryMsg = "The delivery time is: ";
+
+if (isset($_POST["express_delivery"])) {
+    $totalValue += 5;
+//    $deliveryMsg = "Delivery time is ";
+    $delivery_time = date("H:i:s", strtotime("+45 Minutes"));
+
+}
+
+//https://www.w3schools.com/php/keyword_as.asp
+if (isset($_POST["products"])) {
+    foreach ($_POST["products"] as $i => $price) {
+        $totalValue += $products[$i]["price"];
+    }
+}
+
+
+$error = "";
+$email = "";
+$street = "";
+$streetNumber = "";
+$city = "";
+$zipCode = "";
+
+
+$submit = $_POST["submit"];
+$result = true;
+
+// CHECKS IF EMAIL IS VALID
+if (isset($submit)) {
+    if (empty($_POST["email"])) {
+        $error = "email is empty" . "<br>";
+        $result = false;
+    } elseif (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = "Invalid email format";
+        $result = false;
+    } else {
+        setcookie("emailCookie", $_POST["email"]);
+        $email = $_POST["email"];
+    }
+
+// CHECKS IF STREET IS VALID
+    if (isset($_POST["street"])) {
+        $street = $_POST["street"];
+        setcookie("streetCookie", $street);
+        if (empty($street)) {
+            $error .= "Street, Street number, City and Zipcode are required!" . "<br>";
+            $result = false;
+        }
+    }
+
+    //// CHECKS IF STREETNUMBER IS VALID
+    if (isset($_POST["streetnumber"])) {
+        $streetNumber = $_POST["streetnumber"];
+        setcookie("streetNumCookie", $streetNumber);
+        if (!is_numeric($streetNumber)) {
+            $error .= "Street number is not numeric or empty" . "<br>";
+            $result = false;
+        }
+    }
+
+    //// CHECKS IF CITY IS VALID
+    if (isset($_POST["city"])) {
+        $city = $_POST["city"];
+        setcookie("cityCookie", $city);
+        if (empty($city)) {
+            $error .= "Street, Street number, City and Zipcode are required!" . "<br>";
+            $result = false;
+        }
+    }
+
+    // CHECKS IF ZIPCODE IS VALID
+    if (isset($_POST["zipcode"])) {
+        $zipCode = $_POST["zipcode"];
+        setcookie("zipCookie", $zipCode);
+        if (!is_numeric($zipCode)) {
+            $error .= "Zipcode is not numeric or empty" . "<br>";
+            $result = false;
+        }
+    }
+
+    //Order confirmation
+    if (($result == true) && (!empty($totalValue))) {
+        $error = "Thank you, your order has been successfully sent. " . $deliveryMsg . $delivery_time;
+    }
+
+} else {
+    if (!empty($_COOKIE["emailCookie"])) {
+        $email = $_COOKIE["emailCookie"];
+    }
+    if (!empty($_COOKIE["streetCookie"])) {
+        $street = $_COOKIE["streetCookie"];
+    }
+    if (!empty($_COOKIE["streetNumCookie"])) {
+        $streetNumber = $_COOKIE["streetNumCookie"];
+    }
+    if (!empty($_COOKIE["cityCookie"])) {
+        $city = $_COOKIE["cityCookie"];
+    }
+
+    if (!empty($_COOKIE["zipCookie"])) {
+        $zipCode = $_COOKIE["zipCookie"];
+    }
+
+}
+
 
 require 'form-view.php';
